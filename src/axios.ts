e08e1b2186bdfabd74925ca0685be503835f95c3
kolynzb <collinsbenda360@gmail.com>
2023-03-28 23:21:46 +0300

    add base url

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "http://127.0.0.1:8000/api/v1/",
 });
 
 api.interceptors.request.use((config) => {
