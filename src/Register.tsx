c4ecc6c17997c04147de4167f0beb70bed618c40
kolynzb <collinsbenda360@gmail.com>
2023-03-28 09:45:29 +0300

    feat: create register page

---
+++ src/Register.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+type Props = {};
+
+const Register = (props: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="logo">
+        <img
+          src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png"
+          alt=""
+        />
+      </div>
+      <div className="text-center mt-4 name">Twitter</div>
+      <form className="p-3 mt-3" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-field d-flex align-items-center">
+          <span className="far fa-user"></span>
+          <input
+            type="text"
+            name="userName"
+            id="userName"
+            placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-field d-flex align-items-center">
+          <span className="fas fa-key"></span>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+            id="pwd"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-field d-flex align-items-center">
+          <span className="fas fa-key"></span>
+          <input
+            type="password"
+            name="confirmpassword"
+            id="pwd"
+            value={confirmpassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm Password"
+          />
+        </div>
+        <button className="btn mt-3">Register</button>
+      </form>
+      <div className="text-center fs-6">
+        <a href="#">Forget password?</a> or <Link to="/login">Login In</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
