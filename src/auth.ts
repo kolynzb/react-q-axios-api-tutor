b89a942efc68438187ce40b384633066b391bb38
kolynzb <collinsbenda360@gmail.com>
2023-03-28 23:21:35 +0300

    replace urls

--- src/auth.ts
+++ src/auth.ts
@@ -3,9 +3,9 @@ import api from "./axios";
 
 async function login(email: string, password: string) {
   try {
-    const response = await api.post("/auth/login", { email, password });
+    const response = await api.post("/accounts/login/", { email, password });
 
-    const { access_token, refresh_token } = response.data;
+    const { access_token, refresh_token, user } = response.data;
 
     Cookies.set("access_token", access_token, { httpOnly: true });
     Cookies.set("refresh_token", refresh_token, { httpOnly: true });
@@ -13,6 +13,7 @@ async function login(email: string, password: string) {
     // Store the access token in local storage as well, in case you need to access it from a different tab or window
     localStorage.setItem("access_token", access_token);
 
+    // console.log("user:", user);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -36,5 +37,26 @@ async function getCurrentUser() {
     throw error;
   }
 }
+async function register(
+  email: string,
+  password1: string,
+  password2: string,
+  profile_type: string
+) {
+  try {
+    const response = await api.post("/accounts/register/", {
+      email,
+      password1,
+      password2,
+      profile_type,
+    });
+    // TODO: Replace with with Toaster notification.
+    alert(response.data.detail);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
-export { login, logout, getCurrentUser };
+export { login, logout, getCurrentUser, register };
